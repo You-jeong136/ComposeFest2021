3a13e0cd0d1be1d4a44117beefd443592fbc3cba
You-jeong136 <smj06168@gmail.com>
2021-11-07 17:57:22 +0900

    [STYLE] 코드랩 디자인 적용 13 _ Card / 애니메이션

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/codelab/BasicsCodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/codelab/BasicsCodelab2/MainActivity.kt
@@ -5,15 +5,22 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -73,7 +80,64 @@ fun OnboardingScreen(onContinueClicked: () -> Unit){
         }
     }
 }
+//코드랩 13 _ greeting _ Card 랑 기타 애니메이션 적용.
+@Composable
+private fun Greeting(name : String){
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+            CardContent(name)
+    }
+}
+@Composable
+private fun CardContent(name : String){
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+    }
+}
+/*
+//코드랩 1 - 12 까지 했던 것.
 @Composable
 private fun Greeting(name : String){
 
@@ -111,7 +175,7 @@ private fun Greeting(name : String){
         }
     }
 }
-
+*/
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
