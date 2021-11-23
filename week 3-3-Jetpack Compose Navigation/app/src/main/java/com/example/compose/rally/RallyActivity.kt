52e0ce962a51892bb1dd5ffabd52aac8bfe2033f
You-jeong136 <smj06168@gmail.com>
2021-11-23 16:37:56 +0900

    [FEAT] navigate 코드랩 1-4 / 인수탐색

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,17 +19,28 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.theme.RallyTheme
+import com.example.compose.rally.data.UserData
+import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.bills.BillsBody
+import com.example.compose.rally.ui.overview.OverviewBody
+import com.example.compose.rally.RallyScreen.Accounts
+import com.example.compose.rally.RallyScreen.Bills
+import com.example.compose.rally.RallyScreen.Overview
+import com.example.compose.rally.ui.accounts.SingleAccountBody
+
 
 /**
  * This Activity recreates part of the Rally Material Study from
@@ -44,27 +55,80 @@ class RallyActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun RallyApp() {
     RallyTheme {
+
         val allScreens = RallyScreen.values().toList()
-        var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Overview) }
+        // set up navController : nav 생성 & 살아남은 구성 변경을 기억
+        val navController = rememberNavController()
+        val backstackEntry = navController.currentBackStackEntryAsState()
+        //navigation 백 스택 유지 _ 그리고 State로 현재 백스택 엔트리를 제공함.
+        //이 state로, 백스택의 변경사항에 반응가능, 경로에 대한 현재 백스택 항목 쿼리도 가능.
+        val currentScreen = RallyScreen.fromRoute(
+            backstackEntry.value?.destination?.route
+        )
+
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = allScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
-                    currentScreen = currentScreen
+                    onTabSelected = { screen -> navController.navigate(screen.name) },
+                    currentScreen = currentScreen,
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.content(
-                    onScreenChange = { screen ->
-                        currentScreen = RallyScreen.valueOf(screen)
+            val accountsName = RallyScreen.Accounts.name
+            NavHost(
+                navController = navController,
+                startDestination = Overview.name,
+                modifier = Modifier.padding(innerPadding)
+            ) {
+
+                composable(Overview.name) {
+                    OverviewBody(
+                        onClickSeeAllAccounts = { navController.navigate(Accounts.name) },
+                        onClickSeeAllBills = { navController.navigate(Bills.name) },
+                        onAccountClick = { name -> navigateToSingleAccount(navController, name) },
+                    )
+                }
+                composable(Accounts.name) {
+                    AccountsBody(accounts = UserData.accounts) { name ->
+                        navigateToSingleAccount(
+                            navController = navController,
+                            accountName = name
+                        )
                     }
-                )
+                }
+                composable(Bills.name) {
+                    BillsBody(bills = UserData.bills)
+                }
+
+                composable(
+                    "$accountsName/{name}",
+                    arguments = listOf(
+                        navArgument("name"){
+                            type = NavType.StringType
+                        }
+                    )
+                ) { entry ->
+                    //navBackStackEnrty의 매개 중 name
+                    val accountName = entry.arguments?.getString("name")
+                    val account = UserData.getAccount(accountName)
+
+                    //account를 singleAccountBody로 보냄 _ accounts패키지에 구현
+                    SingleAccountBody(account = account)
+                }
+
+
             }
         }
     }
+}
+private fun navigateToSingleAccount(
+    navController: NavHostController,
+    accountName: String
+) {
+    navController.navigate("${Accounts.name}/$accountName")
 }
